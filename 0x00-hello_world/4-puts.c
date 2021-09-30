2a22564ef81ff4b97230cc2d344b6ea0d28579c0
LucasD <3242@holbertonschool.com>
2021-09-30 15:30:41 +0200

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 #include <string.h>
 int main(void)
 {
-char str1[52];
+  char str1[52];
  
-strcpy (str1, "\"Programming is like building a multilingual puzzle");
-puts(str1);
-return(0);
+  strcpy (str1, "\"Programming is like building a multilingual puzzle");
+  puts(str1);
+  return(0);
 }
