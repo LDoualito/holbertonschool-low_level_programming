53cad066af0094a205070bf8e4c75d729fad315e
LucasD <3242@holbertonschool.com>
2021-09-30 15:55:19 +0200

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,5 +8,8 @@ int main(void)
 char str1[52];
 strcpy(str1, "\"Programming is like building a multilingual puzzle");
 puts(str1);
+/**
+ *Return: 1 if the text is good. 0 otherwise
+ */
 return (0);
 }
