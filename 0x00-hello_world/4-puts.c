1eab4c2da6f7b3599f20423af565521c082b59ea
LucasD <3242@holbertonschool.com>
2021-09-30 15:45:39 +0200

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,3 @@
-/**
- * function_name - Short description, single line
- * @parameterx: Description of parameter x
-(* a blank line
- * Description: Longer description of the function)?
-(* section header: Section description)*
- * Return: Description of the returned value
- */
 #include <stdio.h>
 #include <string.h>
 int main(void)
