20f1046bb2c019b8516f949c19030306349b34c5
LucasD <3242@holbertonschool.com>
2021-09-30 15:44:31 +0200

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
+/**
+ * function_name - Short description, single line
+ * @parameterx: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
 #include <stdio.h>
 #include <string.h>
-/*bendo comment*/
 int main(void)
 {
   char str1[52];
