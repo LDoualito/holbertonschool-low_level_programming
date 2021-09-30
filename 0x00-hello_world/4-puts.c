0c8c859683852062b477271418cabe9512ffcbb0
LucasD <3242@holbertonschool.com>
2021-09-30 15:41:33 +0200

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+/*bendo comment*/
 int main(void)
-{ /*bendo bendo*/
-char str1[52];
+{
+  char str1[52];
 strcpy(str1, "\"Programming is like building a multilingual puzzle");
 puts(str1);
 return (0);
