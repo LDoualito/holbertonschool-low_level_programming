c4f8b4e08d3b3e34f1168d624f46076f347859e5
LucasD <3242@holbertonschool.com>
2021-10-01 15:37:50 +0200

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,12 +2,13 @@
 /*more header stuff*/
 
 /*betty style goes brrrr*/
-int main () {
-   char c;
-
-   for(c = 'a' ; c <= 'z' ; c++) {
-      putchar(c);
-   }
-   putchar('\n');
-   return(0);
+int main(void)
+{
+	char c;
+	for(c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
