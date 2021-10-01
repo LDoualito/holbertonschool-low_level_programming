f705fadb047eafc315b16ffb2703c1060f798c06
LucasD <3242@holbertonschool.com>
2021-10-01 16:51:22 +0200

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,7 @@
  *
  * Return: abc...zABC...Z
  */
-int main (void)
+int main(void)
 {
 	char c;
 
@@ -12,7 +12,7 @@ int main (void)
 	{
 		putchar(c);
 	}
-	for(c = 'A' ; c <= 'Z' ; c++)
+	for (c = 'A' ; c <= 'Z' ; c++)
 	{
 		putchar(c);
 	}
