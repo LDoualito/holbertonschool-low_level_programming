4fad84dca028954e72cf9020cfc8c8eb63ad2b52
LucasD <3242@holbertonschool.com>
2021-10-01 12:03:53 +0200

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*more header stuff*/
+
+/*betty style goes brrrr*/
+int main () {
+   int x;
+
+   for(x = 0 ; x < 10 ; x++) {
+	   printf("%d",x);
+   }
+   printf("\n");
+   return(0);
+}
