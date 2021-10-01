a6fc63f90ed04436ec31333f96c4a9ec318f5cbd
LucasD <3242@holbertonschool.com>
2021-10-01 15:59:41 +0200

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,14 @@
 /*more header stuff*/
 
 /*betty style goes brrrr*/
-int main () {
-   
-   int i = 0;
-   for (i = 0; i < 10 ; i++)
-   {
-   putchar(i + '0');
-      }
-   putchar('\n');
-   return(0);
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return(0);
 }
