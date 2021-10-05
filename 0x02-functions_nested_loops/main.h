d251b4cdb2fa9b9cb85e0b84f55d856cdee41710
LucasD <3242@holbertonschool.com>
2021-10-05 16:42:05 +0200

    First use of main.h to print alphabet

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+/**
+ * print_alphabet - display the alphabet
+ *
+ * Return: abc...z
+ */
+int print_alphabet()
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
