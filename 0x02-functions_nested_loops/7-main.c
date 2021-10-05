1f597a556fc7f1cd9741a40d0a6945fd1f0f415c
LucasD <3242@holbertonschool.com>
2021-10-05 20:49:39 +0200

    last digit

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+    print_last_digit(98);
+    print_last_digit(0);
+    print_last_digit(4);
+    print_last_digit(4);
+    _putchar('\n');
+    return (0);
+}
