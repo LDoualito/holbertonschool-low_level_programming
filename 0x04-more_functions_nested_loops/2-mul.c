7d9329e08c0c4bea19471479eb29efc1559ce904
LucasD <3242@holbertonschool.com>
2021-10-07 23:04:23 +0200

    print numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -8,8 +8,6 @@
  */
 int mul (int a,int b)
 {
-	int c;
-
-	c = a * b;
-	return (c);
+	a = a * b;
+	return (a);
 }
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
