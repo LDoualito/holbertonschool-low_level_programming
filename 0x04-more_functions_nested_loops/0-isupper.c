9e9d123cccd1856c43c9f45f5415f5410fff17a0
LucasD <3242@holbertonschool.com>
2021-10-07 22:36:06 +0200

    Upper or no ?

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * _isupper - return 1 if c is uppercase
+ *@c: character
+ * Return: 1 | 0
+ */
 int _isupper(int c)
 {
 	if ( c >= 'A' && c <= 'Z')
