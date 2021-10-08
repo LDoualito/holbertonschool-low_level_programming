99bbf52131dcde9f3ac78c2c7c9c908abcca1b32
LucasD <3242@holbertonschool.com>
2021-10-08 12:18:25 +0200

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i <= n; i++)
+		_putchar('-');
+	_putchar('\n');
+}
