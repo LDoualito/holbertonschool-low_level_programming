a06584bc1e0f2f9e5e5f7ae689c0d46e3635742c
LucasD <3242@holbertonschool.com>
2021-10-08 16:21:34 +0200

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,25 +4,25 @@
 /**
  * main - Fizz buzz
  *
- * Return: Fizz | Buzz \ FizzBuzz | or numbers
+ * Return: Fizz | Buzz | FizzBuzz | or numbers
  */
-int main (void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i%15 == 0)
-			printf ("FizzBuzz");
+		if (i % 15 == 0)
+			printf("FizzBuzz");
 
-		else if ((i%3) == 0)
+		else if ((i % 3) == 0)
 			printf("Fizz");
 
-		else if ((i%5) == 0)
+		else if ((i % 5) == 0)
 			printf("Buzz");
 		else
-			printf("%d",i);
-	        printf(" ");
+			printf("%d", i);
+		printf(" ");
 	}
 	return (0);
 }
