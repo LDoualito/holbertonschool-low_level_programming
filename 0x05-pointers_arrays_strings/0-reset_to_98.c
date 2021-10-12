c61f40ba5663c6bea0c737de6d6f663d5a05298a
LucasD <3242@holbertonschool.com>
2021-10-12 11:11:15 +0200

    Point to a new value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * reset_to_98 - update the value to 98
+ * @*n: pointeurs
+ * Return:n=98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
