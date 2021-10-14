1f13fcca2804808e74659150c3ad79a5d7af058d
LucasD <3242@holbertonschool.com>
2021-10-14 15:31:10 +0200

    put everything on Uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,19 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- *
- *
- *
- *
+ * string_toupper - put everything in uppercase
+ * @n : string
+ * Return: put everything in Uppercase
  */
 char *string_toupper(char *n)
 {
-	int i;
+	int i = 0;
 	int j = strlen(n);
-	
+
 	for (i = 0; i < j; i++)
 	{
-		n[i] = n[i - 32];
+		if ((n[i] >= 97) && (n[i] <= 122))
+		{
+			n[i] = n[i] - 32;
+		}
+
+		else
+		continue;
 	}
 	return (n);
 }
