a82e83a67cf238e49b74fdf17bdc6fcb9f2f003d
LucasD <3242@holbertonschool.com>
2021-10-19 13:59:08 +0200

    copy memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * _memcpy - copy memory area
+ * @dest : memory area output
+ * @src : memory area input
+ * @n : bytes
+ * Return: char
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	memcpy(dest, src, n);
