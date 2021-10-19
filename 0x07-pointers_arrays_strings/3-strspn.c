52d4fe6d160d53f34a18a73f9629adf9d83cfda5
LucasD <3242@holbertonschool.com>
2021-10-19 14:05:08 +0200

    get the same element from a previous string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strspn - get the lenght of a prefix substring
+ * @s : string
+ * @accept : string
+ * Return : char
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len;
+
+	len = strspn(s, accept);
+	return (len);
+}
