68afa0143b502b4f8d508221b1c91368531bd1e8
LucasD <3242@holbertonschool.com>
2021-10-20 20:53:37 +0200

    print a string with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s : string
+ * Return : void
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (i == 0)
+		puts(s);
+
+	i++;
+}
