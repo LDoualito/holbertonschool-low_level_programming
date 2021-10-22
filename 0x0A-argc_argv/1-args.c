61959bb72703dacfd7ed38cdefe00a5e07885fab
LucasD <3242@holbertonschool.com>
2021-10-22 10:55:20 +0200

    numbers of argument pass to the programs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print numbers of argument
+ *@argc: int
+ *@argv: char
+ *Return: void
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
