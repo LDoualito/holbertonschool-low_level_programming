df36c8eb9ae5fc69278e865e10df0312d04f0f61
Doual <3242@holbertonschool.com>
2022-01-31 01:05:47 +0100

    fix 103

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,13 +1,24 @@
 #include "lists.h"
 
-
+/**
+ * get_nodeint_at_index - function that return the nth node
+ * @head: pointer to head of linked list
+ * @index: index of nth node
+ * Return: node or NULL in the case, doesn't exist
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int nm;
+	unsigned int node = 0;
 
-	for (nm = 0; nm < index; nm++)
+	if (head == NULL)
+		return (NULL);
+
+	while (head)
 	{
-
-
-	return (head);
+		if (node == index)
+			return (head);
+		head = head->next;
+		node++;
+	}
+	return (NULL);
 }
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - fx to add a node
+ * @head: head of LL
+ * @idx: index to add the new node
+ * @n: value of new pointer
+ * Return: new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *tmp_head, *prev_node, *new_node;
+	unsigned int counter = 0;
+
+	if (*head == NULL && idx > 0)
+		return (NULL);
+	/* only creating a new pointer, then we allocate and linked to the LL*/
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	/* CASE: create a node at the start */
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	tmp_head = *head;
+	while (tmp_head)
+	{
+		if (counter == idx)
+		{
+			new_node->next = tmp_head;
+			prev_node->next = new_node;
+			return (new_node);
+		}
+		prev_node = tmp_head;
+		tmp_head = tmp_head->next;
+		counter++;
+	}
+	/*case: add a node at the final*/
+	if (counter == idx)
+	{
+		prev_node->next = new_node;
+		return (new_node);
+	}
+	/*erase node when idx is not find it */
+	free(new_node);
+	return (NULL);
+}
